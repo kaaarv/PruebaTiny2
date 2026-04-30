87f99e3b1bea847e5fc582f5160a9eeeff6235fe
Karina Quiros <141165320+kaaarv@users.noreply.github.com>
2026-04-29 22:56:38 -0600

    Refactor output assignments and handle unused inputs
    Updated output assignments and added unused inputs.

--- src/project.v
+++ src/project.v
@@ -17,11 +17,21 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7:4]  = 0; 
   assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_oe[7:1]  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, uio_in[7:1], 1'b0};
+
+  assign uio_oe[0] = 1'b0; // Set all IOs to input mode
+
+  mux_2to1_4b U0 (
+  .a_i(ui_in[3:0]),
+  .b_i(uio_in[7:4]),
+  .s_i(uio_oe[0]),
+  .q_o(uio_out[3:0]) );
+
+
 
 endmodule
